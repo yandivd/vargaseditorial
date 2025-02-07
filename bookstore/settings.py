9f90c0ff2db503afad3c5c9a75bf4daeb57009ed
Yandi Vargas <yandivd@gmail.com>
2025-02-06 22:35:42 -0500

    static root

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 import os
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
