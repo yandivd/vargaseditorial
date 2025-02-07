8e919bab9391e5dfca215f84de5986de079fd538
Yandi Vargas <yandivd@gmail.com>
2025-02-06 22:47:36 -0500

    fixed

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 import os
-STATIC_URL = os.path.join(BASE_DIR, '/store/static/')
-STATIC_ROOT = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/store/static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
